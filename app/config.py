7dca7b3e2a85bd6f06824079f8643e3cd817c3a2
evgen-kld <evgenkld@inbox.ru>
2022-04-06 02:12:54 +0300

    Admin authorization, base custom admin part

--- app/config.py
+++ app/config.py
@@ -3,3 +3,5 @@ class Configuration(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:996062Evgen@localhost/blog_db'
     SECRET_KEY = 'key'
+    SECURITY_PASSWORD_SALT = 'salt'
+    SECURITY_PASSWORD_HASH = 'sha512_crypt'
